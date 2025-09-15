a51db39aad1b22e64a83188d9da39843e9a92827
eigerbot <80028912+giger8848@users.noreply.github.com>
2025-09-15 23:22:31 +0900

    Delete pages/test.js

--- pages/test.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  return res.status(200).json({
-    success: true,
-    message: 'Test API working with correct path!',
-    method: req.method,
-    path: 'pages/api/test.js'
-  });
-}
